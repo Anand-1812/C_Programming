0de2833fa101d68cf251412b8fbe04038521a5bb
Anand-1812 <anand.work2005@gmail.com>
2025-10-14 07:36:21 +0530

    connection with socket

---
+++ sockets/sockets.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define IP "142.251.223.227"
+#define PORT 80
+
+int main() {
+  int s;
+  struct sockaddr_in sock;
+
+  s = socket(AF_INET, SOCK_STREAM, 0);
+  if (s < 0) {
+    printf("socket() error\n");
+    return -1;
+  }
+
+  sock.sin_addr.s_addr = inet_addr(IP);
+  sock.sin_port = htons(PORT);
+  sock.sin_family = AF_INET;
+
+  if (connect(s, (struct sockaddr *)&sock, sizeof(sock)) != 0) {
+    printf("connect() error\n");
+    close(s);
+    return 1;
+  }
+
+  printf("Connected to %s : %d successfully!\n", IP, PORT);
+  close(s);
+
+  return 0;
+}
